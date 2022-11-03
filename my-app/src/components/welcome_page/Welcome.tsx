b2e8c5997a23038485c7fa0eac727c3cc681ac0b
ernest <a1@tcs-MacBook-Air.local>
2022-11-03 19:01:45 +0200

    added first front component

---
+++ my-app/src/components/welcome_page/Welcome.tsx
@@ -0,0 +1,59 @@
+import css from './Welcome.module.css';
+import insta from '../../imgs/insta.svg';
+import youtube from '../../imgs/youtube.svg';
+import fbook from '../../imgs/fbook.svg';
+import globe from '../../imgs/globe.svg';
+
+const Welcome = () => {
+  return (
+    <div className={css.main_div}>
+      <div className={css.label_block}>
+        <p>GREATCHANGE.CHRUCH</p>
+      </div>
+      <div className={css.welcome_block}>
+        <p>ЛАСКАВО ПРОСИМО</p>
+        <div className={css.logo_link}>
+          <a
+            href="https://www.instagram.com/GREATCHANGE.CHURCH/"
+            target="_BLANK"
+            rel="noreferrer"
+          >
+            <img
+              src={insta}
+              alt="insta logo"
+              className={css.btn}
+            />
+          </a>
+          <a
+            href="https://www.youtube.com/c/ЦерковьБольшаяПеремена"
+            target="_BLANK"
+            rel="noreferrer"
+          >
+            <img
+              src={youtube}
+              alt="youtube svg"
+              className={css.btn}
+            />
+          </a>
+          <a
+            href="https://www.facebook.com/greatchange.church"
+            target="_BLANK"
+            rel="noreferrer"
+          >
+            <img
+              src={fbook}
+              alt="facebook svg"
+              className={css.btn}
+            />
+          </a>
+        </div>
+      </div>
+      <div className={css.info_block}>
+        <img src={globe} alt="globe svg" />
+        <p className={css.footer_text}>Зібрання онлайн <br></br>кожної неділі о <strong>10:00</strong> і <strong>12:00</strong></p>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
