f14e068bf9b4b7965c9d6b645e3c7cee81abfd5d
Ernest Myrmyr <ernuk@MacBook-Air.local>
2022-12-08 17:58:35 +0200

    finished church life block. Done with react-slick

--- my-app/src/components/church_life/ChurchLife.tsx
+++ my-app/src/components/church_life/ChurchLife.tsx
@@ -1,46 +1,89 @@
 import Slider from "react-slick";
 import "./slick.css";
 import "./slick-theme.css";
+import './church_life.css';
 import bros from '../../imgs/bros.png';
 import sisters from '../../imgs/sisters.png';
 import youth from '../../imgs/youth.png';
-import css from './Sliding.module.css';
+import css from './ChurchLife.module.css';
+
+type PropsType = {
+  className?: string;
+  onClick?: React.MouseEventHandler;
+};
+
+const SampleNextArrow = (props: PropsType) => {
+  const { className, onClick } = props;
+
+  return (
+    <div
+      className={className}
+      onClick={onClick}
+    />
+  );
+};
+
+const SamplePrevArrow = (props: PropsType) => {
+  const { className, onClick } = props;
+
+  return (
+    <div
+      className={className}
+      onClick={onClick}
+    />
+  );
+};
 
 const ChurchLife = () => {
   const settings = {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
 
   return (
     <div className={css.main_div}>
-      <h1 className={css.caption}>ЖИТТЯ ЦЕРКВИ</h1>
+      <div className={css.background}></div>
 
-      <Slider {...settings}>
-        <div className={css.slider_div}>
-          <img
-            src={bros}
-            alt="brothers img"
-            className={css.slider_img}
-          />
-        </div>
-        <div className={css.slider_div}>
-          <img
-            src={sisters}
-            alt="sisters img"
-            className={css.slider_img}
-          />
-        </div>
-        <div className={css.slider_div}>
-          <img
-            src={youth}
-            alt="youth img"
-            className={css.slider_img}
-          />
-        </div>
-      </Slider>
+      <div className={css.content_block}>
+        <h1 className={css.caption}>ЖИТТЯ ЦЕРКВИ</h1>
+
+        <Slider {...settings}>
+          <div className={css.slider_div}>
+            <div>
+              <img
+                src={bros}
+                alt="brothers img"
+                className={css.slider_img}
+              />
+              <p>БРАТЕРСТВО</p>
+            </div>
+          </div>
+          <div className={css.slider_div}>
+            <div>
+              <img
+                src={sisters}
+                alt="sisters img"
+                className={css.slider_img}
+              />
+              <p>СЕСТРІВСТВО</p>
+            </div>
+          </div>
+          <div className={css.slider_div}>
+            <div>
+              <img
+                src={youth}
+                alt="youth img"
+                className={css.slider_img}
+              />
+              <p>МОЛОДЬ</p>
+            </div>
+          </div>
+        </Slider>
+      </div>
     </div>
   );
 };
